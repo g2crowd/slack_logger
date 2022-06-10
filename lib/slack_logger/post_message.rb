035842d5ea60eb69a7e23ca52162331d048a9987
Evgeny Karpov <ekarpov@g2.com>
2022-06-10 14:59:18 -0500

    feat: Add channel suffix for non-production environments
    PA-1337

--- lib/slack_logger/post_message.rb
+++ lib/slack_logger/post_message.rb
@@ -1,5 +1,15 @@
 module SlackLogger
   class PostMessage
+    CHANNEL_SUFFIX_DELIMITER = '-'.freeze
+
+    ENV_CHANNEL_SUFFIX_MAPPING =
+    {
+      nil => '',
+      production: '',
+      development: "#{CHANNEL_SUFFIX_DELIMITER}dev",
+      staging: "#{CHANNEL_SUFFIX_DELIMITER}stg"
+    }.freeze
+
     def self.call(**args)
       new(**args).call
     end
@@ -8,19 +18,38 @@ module SlackLogger
 
     def initialize(text:, channel: SlackLogger.config.channel, attachments: [])
       @text = text
-      @channel = channel
+      @channel = env_channel(channel)
       @attachments = attachments
     end
 
     def call
-      return unless SlackLogger.config.enabled.call(channel: channel, text: text)
-      client.chat_postMessage channel: channel, text: text, attachments: attachments
+      do_call
+    rescue Slack::Web::Api::Errors::ChannelNotFound
+      raise if SlackLogger.config.production?
+      logger.warn "attempt to send to non-existent slack channel #{channel.inspect}"
     end
 
     private
 
+    def do_call
+      return unless SlackLogger.config.enabled.call(channel: channel, text: text)
+      client.chat_postMessage channel: channel, text: text, attachments: attachments
+    end
+
     def client
       Slack::Web::Client.new(token: SlackLogger.config.token)
+    end
+
+    def env_channel(channel)
+      "#{channel}#{ENV_CHANNEL_SUFFIX_MAPPING[env] || "#{CHANNEL_SUFFIX_DELIMITER}#{env}"}"
+    end
+
+    def env
+      SlackLogger.config.env
+    end
+
+    def logger
+      @logger ||= SlackLogger.config.logger || Logger.new(IO::NULL)
     end
   end
 end
--- lib/slack_logger/version.rb
+++ lib/slack_logger/version.rb
@@ -1,3 +1,3 @@
 module SlackLogger
-  VERSION = '1.0.0'.freeze
+  VERSION = '1.0.1'.freeze
 end
